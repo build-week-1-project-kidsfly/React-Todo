f05e6c048526dd8181a6dd27eb3e1c81da2c1fce
josepaniagua <jpanjr@gmail.com>
2019-12-04 17:53:51 -0800

    added clickhandler to todo file

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Todo = (props) => {
-    const { groceryItem } = props;
+    const { groceryItem, toggleItem } = props;
 
-
-
-
+    const clickHandler = () => {
+        toggleItem(groceryItem.id)
+    };
     return (
-        <div className={} onClick={}>
+        <div className={`item${groceryItem.purchased ? " purchased" : ""}`}
+             onClick={clickHandler}>
             <p>
                 {groceryItem.name}
             </p>
